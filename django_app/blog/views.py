d3aa1ffb21bb375437f7642fafc901504f1c75d9
LimHM <gusals3407@gmail.com>
2017-02-07 06:19:44 +0900

    post_list 함수 작성

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    # request를 넘겨받아 render 메서드 호출
+    # render 메서드는 넘겨진 요청(request)과 blog/post_list.html 템플릿 받아
+    # 리턴된 내용을 브라우저에 보여줌
+    return render(request, 'blog/post_list.html', {})
