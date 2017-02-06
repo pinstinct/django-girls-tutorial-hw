3a0b079fa39a3c8bab1a42c36dfe336077aaf475
LimHM <gusals3407@gmail.com>
2017-02-07 06:06:13 +0900

    url 등록

--- django_app/django_girls/urls.py
+++ django_app/django_girls/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    # admin/ 으로 시작하는 모든 URL을 view와 대조해 찾아냄
     url(r'^admin/', admin.site.urls),
+
+    # http://127.0.0.1:8000/ 로 들어오는 모든 접속요청을 blog.urls로 전송
+    url(r'', include('blog.urls')),
 ]
